f5854cfe82a658f86104ca144934654ed1df232f
Francesco Bianco <info.francescobianco@gmail.com>
2017-12-17 23:21:00 +0100

    keystone

--- web/routes/middleware.js
+++ web/routes/middleware.js
@@ -21,6 +21,8 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
+		{ label: 'Contact2', key: 'contact', href: '/contact' },
+		{ label: 'Contact4', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
 	next();
